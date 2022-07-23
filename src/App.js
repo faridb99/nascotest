40a455a437eb7f456438826e8d66c446bf9f8a91
Farid Boulos <Farid.Boulos@live.com>
2022-07-23 12:30:53 +0300

    Added  Main Table with local employee Data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import logo from "./logo.svg";
+import data from "./mock-data.json";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [employees, setEmployees] = useState(data);
+
   return (
     <div className="App">
-      <table></table>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job Title</th>
+            <th>Department</th>
+            <th>Location</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {employees.map((contact) => (
+            <tr>
+              <td>{contact.fullname}</td>
+              <td>{contact.jobtitle}</td>
+              <td>{contact.department}</td>
+              <td>{contact.location}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
